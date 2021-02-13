f610b2cab26c53849cb9e16d48f4c5ed37a6ab25
ckoch786 <yunusabd786@gmail.com>
2021-02-12 23:25:43 -0500

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,24 @@ class Portfolio < ApplicationRecord
     def self.angular
         where(subtitle: 'Angular')
     end
-
+    
+    # -> {} is ruby lambda
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+    # for non 'static' defaults do this instead of setting defaults on migration
+    # this call back is made after Portfolio.new '#new'
+    # after_create happens after #create
+    after_initialize :set_defaults
+    
+    # self => this
+    def set_defaults
+        # ||= is if null then assign
+        # shortcut for 
+        # if self.main_image == nil 
+        #    self.main_image = "http://place..."
+        # end
+        # if only used = it would override the existing values
+        self.main_image ||= "http://placehold.it/600x400"
+        self.thumb_image ||= "http://placehold.it/350x200"
+    end
 end
