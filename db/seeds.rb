9320beed62cfd5034a12a37eb7955cc0619735dc
ckoch786 <yunusabd786@gmail.com>
2021-02-13 15:43:12 -0500

    Integrated techonolies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,8 @@ puts "5 skills posts created"
         title: "Portfolio title: #{portfolio_item}",
         subtitle: "Ruby on Rails",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        main_image: "http://placehold.it/600x400",
+
+            main_image: "http://placehold.it/600x400",
         thumb_image: "http://placehold.it/350x200",
     )
 end
@@ -53,3 +54,12 @@ end
     )
 end
 puts "9 portfolio items created"
+
+3.times do |technology|
+    # new syntax
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}"
+    )
+end
+
+puts "3 technologies created"
