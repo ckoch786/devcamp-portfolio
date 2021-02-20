1ecdd48bed82d48e72df05791daeed9fc1388a7b
ckoch786 <yunusabd786@gmail.com>
2021-02-20 15:59:43 -0500

    Added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # rails 5 has protect_from_forgery with: :exception
-    
   include DeviseWhitelist
+  
+  #better to do this in a concern
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
